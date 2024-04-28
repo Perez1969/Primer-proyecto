d5983468cdde9c741a213b7329e29d4f7eeb6eba
Perez1969 <1969josealberto@gmail.com>
2024-04-28 08:39:19 +0200

    primer commit

---
+++ bin/Prueba1.py
@@ -0,0 +1,8 @@
+# Archivo : ejemplo1 _1.py
+#Autor:xxxxx xxxxxxx(nombre y apellidos)
+#Fecha: DD/MM/AAAA
+#Descripción:Imprimir por pantalla la frase
+#"Aprender a programar es divertido"
+#Cuerpo del programa
+print ('Aprender a programar es divertido')
+
---
+++ src/Prueba1.py
@@ -0,0 +1,7 @@
+# Archivo : ejemplo1 _1.py
+#Autor:xxxxx xxxxxxx(nombre y apellidos)
+#Fecha: DD/MM/AAAA
+#Descripción:Imprimir por pantalla la frase
+#"Aprender a programar es divertido"
+#Cuerpo del programa
+print ('Aprender a programar es divertido')
