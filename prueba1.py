44850319e634725f7e0b9144d0a8c379fbc9e4cd
Perez1969 <1969josealberto@gmail.com>
2024-04-28 22:55:31 +0200

    prueba de uso de funciones

--- prueba1.py
+++ prueba1.py
@@ -15,3 +15,15 @@ imprimir = True
 if imprimir:    
     print(x, miedad)
     # salida : el valor de mi edad = a-b
+#practica de funciones
+def en_pantalla(frase1, frase2):
+ print(frase1, frase2)
+en_pantalla('Me gusta',  'Python')
+             
+
+             
+
+             
+
+
+
