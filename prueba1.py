0d0c799f857d5b11f318c3d1b2ddd7d670e23b70
Perez1969 <1969josealberto@gmail.com>
2024-04-28 20:08:06 +0200

    seguimos aprendiendo

--- prueba1.py
+++ prueba1.py
@@ -1,3 +1,17 @@
 print("hola soy Jose") 
 print("es mi primera vez con Python")
 print("estoy practicando el comando print")
+#"""esto es una pueba de escritura de comentarios de varias líneas sobre una operacion matematica"""
+# cálculo de mi edad
+ # definimos edad con una variable x con una cadena
+x ="el valor de mi edad =a-b es "
+#"a es año actual y b es año de nacimiento"
+a, b =2024, 1969
+#realizamos  una operación  con a y b
+c = a- b
+# definimos una variable booleana
+imprimir = True
+# si imprimir   , print()
+if imprimir:
+    print(x, c)
+    # salida : el valor de mi edad = a-b
